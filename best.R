f7db6b0ed3340806325d56a970c1ef3593cb048c
PATRICK AYER <PATRICK.AYER35@GMAIL.COM>
2017-11-16 15:56:53 -0800

    code for extracting the relevant data

--- best.R
+++ best.R
@@ -6,8 +6,32 @@ best <- function(state, outcome)
 	{
 		stop("invalid state")
 	}
-	if ((outcome != "heart attack") |(outcome != "heart failure") | (outcome != "pneumonia"))
+	if ((outcome != "heart attack") | (outcome != "heart failure") | (outcome != "pneumonia"))
 	{
 		stop("invalid outcome")
 	}
+	
+	# extract data from data frame that pertains to specified state
+	state_data <- data[data$State == state, ]
+	
+	# create a vector of the relevant column values:
+	# Hospital.Name (col 2), State (col 7)
+	# if heart attack, col 11
+	# if heart failure, col 17
+	# if pneumonia, col 23
+	
+	if (outcome == "heart attack")
+	{
+		outcome_col <- which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")
+	}
+	else if (outcome == "heart failure")
+	{
+		outcome_col <- which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")
+	}
+	else
+	{
+		outcome_col <- which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+	}
+	
+	rel_data <- state_data[, c(2, outcome_col)]
 }
