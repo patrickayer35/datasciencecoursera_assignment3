0be798d9b04f825cb38c0abb6caa3ae7452f7c85
PATRICK AYER <PATRICK.AYER35@GMAIL.COM>
2017-11-28 13:21:43 -0800

    final best.R function

--- best.R
+++ best.R
@@ -1,14 +1,11 @@
 best <- function(state, outcome)
 {
 	raw_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-	#data <- read.csv("outcome-of-care-measures.csv")
 	states <- unique(raw_data$State)
 	if (!(state %in% states))
 	{
 		stop("invalid state")
 	}
-	#print(outcome)
-	#print(outcome == "heart attack")
 	if (outcome != "heart attack")
 	{
 		if (outcome != "heart failure")
@@ -20,79 +17,52 @@ best <- function(state, outcome)
 		}
 	}
 	
-	# extract data from data frame that pertains to specified state
-	state_data <- data[data$State == state, ]
-	
-	# create a vector of the relevant column values:
-	# Hospital.Name (col 2)
-	# if heart attack, col 11
-	# if heart failure, col 17
-	# if pneumonia, col 23
+	raw_state_data <- raw_data[raw_data$State == state, ]
 	
 	if (outcome == "heart attack")
 	{
-		rel_data <- state_data[, c(2, which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"))]
-		#rel_data <- rel_data[order(rel_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ]
+		state_data <- raw_state_data[, c(2, which(colnames(raw_state_data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"))]
 	}
 	else if (outcome == "heart failure")
 	{
-		rel_data <- state_data[, c(2, which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"))]
-		#print(head(rel_data, 50))
-		#rel_data <- rel_data[order(rel_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ]
+		state_data <- raw_state_data[, c(2, which(colnames(raw_state_data) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"))]
 	}
 	else
 	{
-		rel_data <- state_data[, c(2, which(colnames(data) == "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"))]
-		#rel_data <- rel_data[order(rel_data$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ]
+		state_data <- raw_state_data[, c(2, which(colnames(raw_state_data) == "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"))]
 	}
-	colnames(rel_data) <- c("hospital", "rate")
-	rel_data$rate <- as.numeric(rel_data$rate)
-	#print(length(rel_data$rate))
-	clean_data <- data.frame(rel_data[complete.cases(rel_data),])
-	#print(length(clean_data$rate))
-	#print(head(clean_data, 50))
-	hospitals <- best_hosp(clean_data)
-	#print(hospitals)
-	return(hospitals[1])
+	
+	colnames(state_data) <- c("hospital", "rate")
+	state_data$rate <- as.numeric(state_data$rate)
+	clean_data <- data.frame(state_data[complete.cases(state_data),])
+	ordered_data <- clean_data[order(clean_data$rate),]
+	print(head(ordered_data))
+	return(best_hospital(ordered_data))
 }
 
-best_hosp <- function(df)
-{
-	smallest <- 101
-	for (row in 1:length(df$hospital))
-	{
-		#print("line 64")
-		if (df[row, 2] < smallest)
-		{
-			smallest = df[row, 2]
-		}
-	}
-	print(smallest)
+best_hospital <- function(df)
+{	
+	target_rate <- df[1, 2]
+	match <- target_rate
 	hospitals <- vector("character")
-	for (row in 1:length(df$hospital))
+	i <- 1
+	while (match == target_rate)
 	{
-		#if (df[row, df$rate] == smallest)
-		if (df[row, which(colnames(df) == "rate")] == smallest)
-		{
-			hospitals <- c(hospitals, df[row, which(colnames(df) == "hospital")])
-		}
-	}
-	return(sort(hospitals))
-}
-
-find_smallest_bad <- function(df)
-{
-	best_hospitals <- df[1, 1]
-	smallest <- df[1, 2]
-	i = 2
-	s <- df[i, 2]
-	while ((!is.na(s)) & (s == smallest))
-	{
-		best_hospitals <- c(best_hospitals, df[i, 1])
+		hospitals <- c(hospitals, df[i, 1])
 		i <- i + 1
-		s <- df[i, 2]
+		match <- df[i, 2]
 	}
-	return(sort(best_hospitals))
+	if (length(hospitals) == 1)
+	{
+		return(hospitals)
+	}
+	else
+	{
+		#print("all hospitals")
+		#print(hospitals)
+		order_hospitals <- sort(hospitals)
+		return(order_hospitals[1])
+	}
 }
 
 
